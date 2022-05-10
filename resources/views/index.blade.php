3f82440ae58dcaa08f0c488bfdf266903b5bbc27
janroeNabepero <janroe@nabepero.co.jp>
2022-05-10 21:22:35 +0800

    hardcoded recently reviewed, most anticipated and coming soon sections

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -241,6 +241,86 @@
                     Recently Reviewed
                 </h2>
                 <!-- <livewire:recently-reviewed> -->
+
+                <div class="recently-reviewed-container space-y-12 mt-8">
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="{{ asset('ff7.jpg') }}" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full -mr-5 -mb-5">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Final Fantasy VII Remake
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                Playstation 4
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                                Set in the dystopian cyberpunk metropolis of Midgar, players control mercenary Cloud Strife.
+                                He joins AVALANCHE, an eco-terrorist group trying to stop the powerful megacorporation Shinra from using the planet's life essence as an energy source.
+                                The gameplay combines real-time action with strategic and role-playing elements.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="{{ asset('animalcrossing.jpg') }}" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full -mr-5 -mb-5">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Animal Crossing: New Horizons
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                Nintendo Switch
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                                In New Horizons, the player controls a character who moves to a deserted island after purchasing a getaway package from Tom Nook,
+                                playing the game in a nonlinear fashion and developing the island as they choose.
+                                They can gather and craft items, customize the island, and develop it into a community of anthropomorphic animals.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="{{ asset('doom.jpg') }}" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full -mr-5 -mb-5">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Doom Eternal
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                Playstation 4, PC
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                            Hell’s armies have invaded Earth.
+                            Become the Slayer in an epic single-player campaign to conquer demons across dimensions and stop the final destruction of humanity.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
 
             <div class="flex flex-col lg:w-1/4 mr-32">
@@ -250,6 +330,56 @@
                         Most Anticipated
                     </h2>
                     <!-- <livewire:most-anticipated> -->
+                    <div class="most-anticipated-container space-y-10 mt-8">
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ asset('cyberpunk.jpg') }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Cyberpunk 2077
+                                </a>
+                                <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ asset('avengers.jpg') }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Marvel's Avengers
+                                </a>
+                                <div class="text-gray-400 text-sm mt-1">Sept 3, 2020</div>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ asset('ghost.jpg') }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Ghost of Tsushima
+                                </a>
+                                <div class="text-gray-400 text-sm mt-1">July 17, 2020</div>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ asset('tlou2.jpg') }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    The Last of Us Part II
+                                </a>
+                                <div class="text-gray-400 text-sm mt-1">June 19, 2020</div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
 
                 <div class="coming-soon py-12">
@@ -257,6 +387,57 @@
                         Coming Soon
                     </h2>
                     <!-- <livewire:coming-soon> -->
+
+                    <div class="coming-soon-container space-y-10 mt-8">
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ asset('cyberpunk.jpg') }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Cyberpunk 2077
+                                </a>
+                                <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ asset('avengers.jpg') }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Marvel's Avengers
+                                </a>
+                                <div class="text-gray-400 text-sm mt-1">Sept 3, 2020</div>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ asset('ghost.jpg') }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Ghost of Tsushima
+                                </a>
+                                <div class="text-gray-400 text-sm mt-1">July 17, 2020</div>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ asset('tlou2.jpg') }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    The Last of Us Part II
+                                </a>
+                                <div class="text-gray-400 text-sm mt-1">June 19, 2020</div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             <!-- End of Most Anticipated and Coming Soon Section -->
             </div>
