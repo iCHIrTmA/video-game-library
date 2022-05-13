312a3f033c539100b2b48e15712b6c5a208ed5a9
janroeNabepero <janroe@nabepero.co.jp>
2022-05-14 07:24:26 +0800

    refactor single game page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,9 +2,11 @@
 @section('content')
     <div class="container mx-auto px-4">
         <div class="game-details border-b border-gray-800 pb-12 flex flex-col lg:flex-row">
-            <div class="flex-none mx-auto lg:mx-0">
-                <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="cover">
-            </div>
+            @isset($game['cover']))
+                <div class="flex-none mx-auto lg:mx-0">
+                    <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="cover">
+                </div>
+            @endisset
             <div class="lg:ml-12 lg:mr-64">
                 <h2 class="font-semibold text-4xl leading-tight mt-5 lg:mt-0">{{ $game['name'] }}</h2>
                 <div class="text-gray-400">
@@ -14,13 +16,23 @@
                         @endforeach
                     </span>
                     &middot;
-                    <span>
-                        {{ $game['involved_companies'][0]['company']['name'] }}
-                    </span>
+                    @isset($game['involved_companies'])
+                        <span>
+                            {{ $game['involved_companies'][0]['company']['name'] }}
+                        </span>
+                    @else
+                        <span>
+                            a brave company
+                        </span>
+                    @endisset
                     &middot;
                     <span>
                         @foreach ($game['platforms'] as $platform)
-                            {{ $platform['abbreviation'] }},
+                            @isset($platform['abbreviation'])
+                                {{ $platform['abbreviation'] }},
+                            @else
+                                Surprise Platform
+                            @endisset
                         @endforeach
                     </span>
                 </div>
@@ -73,14 +85,16 @@
                     {{ $game['summary'] }}
                 </p>
 
-                <div class="mt-12">
-                    <a
-                        href="https://youtube.com/watch?v={{ $game['videos'][0]['video_id'] }}"
-                        class="inline-flex bg-blue-500 text-white font-semi-bold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
-                        <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
-                        <span>Play Trailer</span>
-                    </a>
-                </div>
+                @isset($game['videos'])
+                    <div class="mt-12">
+                        <a
+                            href="https://youtube.com/watch?v={{ $game['videos'][0]['video_id'] }}"
+                            class="inline-flex bg-blue-500 text-white font-semi-bold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
+                            <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
+                            <span>Play Trailer</span>
+                        </a>
+                    </div>
+                @endisset
             </div>
         </div>
         <!-- End Game Details -->
