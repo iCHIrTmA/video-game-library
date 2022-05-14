86248b829cf2746b577adbbb497d91e9e139d452
janroeNabepero <janroe@nabepero.co.jp>
2022-05-14 11:48:00 +0800

    refactor show game controller method and single game page

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class GamesController extends Controller
 {
@@ -25,6 +26,46 @@ class GamesController extends Controller
 
         abort_if(!$game, 404);
 
-        return view('show', ['game' => $game[0]]);
+        return view('show', ['game' => $this->formatForView($game[0])]);
+    }
+
+    private function formatForView($game)
+    {
+        return collect($game)->merge([
+            'coverImageUrl' => Str::replaceFirst('thumb', 'cover_big', $game['cover']['url'] ),
+            'genres' => collect($game['genres'])->implode('name', ', '),
+            'involvedCompanies' => isset($game['involved_companies'][0]['company']['name']) ?? null,
+            'platforms' => isset($game['platforms']) ? collect($game['platforms'])->implode('abbreviation', ', ') : null,
+            'memberRating' => isset($game['rating']) ? round($game['rating']) . '%' : null,
+            'criticRating' => isset($game['aggregated_rating']) ? round($game['aggregated_rating']) . '%' : null,
+            'trailer' =>  isset($game['videos'][0]['video_id']) ? 'https://youtube.com/watch?v=' . $game['videos'][0]['video_id'] : '#',
+            'screenshots' => collect($game['screenshots'])->map(function ($screenshot) {
+                return [
+                    'big' => Str::replaceFirst('thumb', 'screenshot_big', $screenshot['url']),
+                    'huge' => Str::replaceFirst('thumb', 'screenshot_huge', $screenshot['url']),
+                ];
+            })->take(9),
+            'social' => [
+                'website' => collect($game['websites'])->first(),
+                'facebook' => collect($game['websites'])->filter(function ($website) {
+                    return Str::contains($website['url'], 'facebook');
+                })->first(),
+                'twitter' => collect($game['websites'])->filter(function ($website) {
+                    return Str::contains($website['url'], 'twitter');
+                })->first(),
+                'instagram' => collect($game['websites'])->filter(function ($website) {
+                    return Str::contains($website['url'], 'instagram');
+                })->first(),
+            ],
+            'similarGames' => collect($game['similar_games'])->map(function ($similar) {
+                return [
+                    'slug' => $similar['slug'],
+                    'coverImageUrl' => isset($similar['cover']) ? Str::replaceFirst('thumb', '720p', $similar['cover']['url']) : 'https://via.placeholder.com/264x352',
+                    'rating' => isset($similar['rating']) ? round($similar['rating']) . '%' : null,
+                    'name' => $similar['name'],
+                    'platforms' => isset($similar['platforms']) ? collect($similar['platforms'])->implode('abbreviation', ', ') : null,
+                ];
+            })->take(6),
+        ]);
     }
 }
