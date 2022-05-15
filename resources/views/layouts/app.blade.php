a38b21f1d2cf00f3e01c804e73d182e43269be7b
janroeNabepero <janroe@nabepero.co.jp>
2022-05-15 15:20:20 +0800

    npm run prod, artisan optimize and games nav bar to index page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
                     <img src="{{ asset('logo.svg')}}" alt="corderue-logo" class="w-32 flex-none">
                 </a>
                 <ul class="flex ml-0 lg:ml-16 space-x-8 mt-6 lg:mt-0">
-                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="/" class="hover:text-gray-400">Games</a></li>
                     <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
                     <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
                 </ul>
